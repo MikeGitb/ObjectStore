d665829cb35092db421ca011b7d32673189d7ea4
MikeGitb <MikeGitb@users.noreply.github.com>
2018-02-05 20:20:05 +0100

    [Examples] New basic usage example, that reads less like a series of tests

--- examples/basic_use.cpp
+++ examples/basic_use.cpp
@@ -4,44 +4,49 @@
 #include <string>
 #include <atomic>
 #include <cassert>
+#include <vector>
 
 using namespace std;
 using namespace mgb;
 
 
 struct MyType {
-	static std::atomic_int i;
 	std::string message;
-	MyType() { i++; }
+	MyType(const MyType& other) = delete;
 	MyType(std::string s)
 		: message(std::move(s))
 	{
-		i++;
 	}
-
-	~MyType()
-	{
-		i--;
-	}
-	static int getCount() { return i; }
 };
 
-std::atomic_int MyType::i{ 0 };
+struct Generator {
+	int i = 0;
+	int operator()() {
+		return i++;
+	}
+};
 
 int main() {
+	std::vector<sos::ConstHandle<MyType>> queue1;
+	std::vector<sos::ConstHandle<MyType>> queue2;
 
+	Generator gen;
 	sos::SharedObjectStore<MyType,10> store;
-	{
-		auto handle1 = store.create("Hello1");
-		auto handle2 = store.create("Hello2");
-		assert(MyType::getCount() == 2);
-		handle2->message = "Hello World";
 
-		auto chandle1 = std::move(handle1).lock();
-		auto chandle2 = chandle1;
-		assert(chandle1->message == "Hello1");
-		assert(chandle2->message == "Hello1");
+	while (store.live_objects_approx() < 10) {
+		auto h = store.create(std::to_string(gen()));
+		h->message = "Hello" + h->message;
+		auto ch = std::move(h).lock();
+		queue1.push_back(ch);
+		queue2.push_back(ch);
 	}
-	assert(MyType::getCount() == 0);
-	std::cout << "Complete!" << std::endl;
+	{
+		for (const auto& e : queue1) {
+			std::cout << e->message << std::endl;
+		}
+		for (const auto& e : queue2) {
+			std::cout << e->message << std::endl;
+		}
+	}
+
 }
